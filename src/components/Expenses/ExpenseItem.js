f8ef25b8e9730de3464f7d867011812808e9ce82
Levente Ibrányi <levente.ibranyi@dpdgroup.com>
2022-09-26 10:44:29 +0200

    Event listeners basic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,13 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
+  /* function clickHandler() {
+    console.log("Clicked!!!");
+  } */
+  const clickHandler = () => {
+    console.log("Clicked!!!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -9,6 +16,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={() => {console.log("clicked");}}>Change Title</button> */}
     </Card>
   );
 }
